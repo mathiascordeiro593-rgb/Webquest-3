5f581c0b62432a2d667eb6eaeb2db02236bec36f
mathiascordeiro593-rgb <mathiascordeiro593@gmail.com>
2025-11-22 18:14:54 -0300

    Create Contato.java

---
+++ Contato.java
@@ -0,0 +1,44 @@
+// Integrantes da equipe:
+// Mathias Falcão Rico da Silva Cordeiro de Oliveira
+// Márcia de Oliveira Brito Santos
+
+public class Contato {
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Contato(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Telefone: " + telefone + ", Email: " + email;
+    }
+}
